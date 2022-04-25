8177124c041a19813c68fdad7addb0c1c61cc797
Cristian-DP <cristian.pereyra@mi.unc.edu.ar>
2022-04-24 22:38:11 -0300

    - Feat: isCapicua completed

--- practicos/claseII/README.md
+++ practicos/claseII/README.md
@@ -1,14 +1,23 @@
 ### BootCamp Java 2022
 
-# Enunciado
-
 - [] Entero capicua
 - [] Factorial
 - [] Decimal a Binario
 - [] Binario a Decimal
 
 ## Entero Capicua
-Construir un metodo que reciba un entero como parametro y devuelva boolean indicando si es capicua o no dicho numero.
+*Construir un metodo que reciba un entero como parametro y devuelva boolean indicando si es capicua o no dicho numero.*
+
+Recordemos que **capicúa** se refiere a cualquier numero que se lee igual de izquierda a derecha que de derecha a izquierda. Ejemplos: 1221, 52125
+
+Para su resolución se procedió como sigue
+
+	1. Se recibe una variable int
+	2. La variable int es convertida a un array de string
+	3. Se lee el array tanto de izquiera a derecha como de derecha a izquierda
+		3.1. Mientras que las lecturas sean iguales, sigo.
+		3.2. Si la lecturas son distintas, retorno **false**.
+	4. Cuando los indices coinciden y se cumplieron todas las igualdades, retorno **true**. 
 
 ## Factorial
 Construir un Metodo que recibe un entero y devuelve el factorial del mismo.
--- practicos/claseII/src/packet/Main.java
+++ practicos/claseII/src/packet/Main.java
@@ -2,9 +2,47 @@ package packet;
 
 public class Main {
 	
+	/**
+	 * Al recibir un numero entero, se convierte a String. 
+	 * Se recorre el String an ambas direcciones 
+	 * (de derecha a izquierda y de izquierda a derecha).
+	 * Si no se cumple la condicion de igualdad en cada lectura, se retorna false.
+	 * 
+	 * @param number
+	 * @return boolean
+	 */
+	private boolean isCapicua (int number) {
+		
+		/* Un digito*/
+		if (number < 10)
+			return true;
+		
+		String s;
+		int i, j;
+		
+		/* Inicializo variables */
+		s = Integer.toString(number);
+		j = s.length() - 1;
+		i = 0;
+		
+		/* Mas de un digito */
+		for (; i != j && j > i; i++, j--)
+			if (s.charAt(i) != s.charAt(j))
+				return false;
+		
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		
-
+		Main m = new Main();
+		
+		/* Prueba capicua */
+		System.out.println( m.isCapicua(44) ? "true" : "false" );
+		System.out.println( m.isCapicua(1001) ? "true" : "false" );
+		System.out.println( m.isCapicua(1100) ? "true" : "false" );
+		System.out.println( m.isCapicua(25632) ? "true" : "false" );
+		
 	}
 
 }
