52641149dbea0fd2789d00d4ee92bc51c8fedbce
Cristian-DP <cristian.pereyra@mi.unc.edu.ar>
2022-05-01 22:48:43 -0300

    - Feat: Implementacion de recursion para calcular Factorial

--- practicos/PracticoClaseIII/src/recursion/FactorialRecursion.java
+++ practicos/PracticoClaseIII/src/recursion/FactorialRecursion.java
@@ -1,18 +1,20 @@
-package factorialRecursion;
+package recursion;
 
 public class FactorialRecursion {
 	
 	public long factorialRecursion (int number) {
-		return 1;
-	
+		if (number == 1)
+			return 1;
+		
+		return number * factorialRecursion(number-1);
 	}
 	
 	public static void main(String[] args) {
 		FactorialRecursion f = new FactorialRecursion();
-		int number;
 		
-		number = 4;
-		System.out.println(number + " -> " + f.factorialRecursion(number));
+		System.out.println( 4 + " -> " + f.factorialRecursion(4));
+		System.out.println( 5 + " -> " + f.factorialRecursion(5));
+		System.out.println( 40 + " -> " + f.factorialRecursion(40));
 		
 
 	}
